0118d4dc8800346257a863eecc9b4e41077b69af
Miguel Angel Fernandez Sanchez <mafesan@delta15.aulas.gsyc.urjc.es>
2016-10-14 12:53:06 +0200

    ejercicio c - p2

--- p2c.asm
+++ p2c.asm
@@ -1,5 +1,5 @@
 	.data
-
+str: .asciiz "El resultado es: \n"
 	.text
 main: 
 	# Introducir primer entero por pantalla
@@ -11,6 +11,10 @@ main:
 	syscall
 	move $t1, $v0 # Mover al registro temporal t1
 
+	la $a0, str # Imprime cadena de caracteres
+	li $v0, 4
+	syscall
+	
 	add $a0, $t0, $t1 # Suma y lo deja en $a0 (registro del que lee el syscall "print integer")
 	
 	li $v0, 1 # Imprime resultado por pantalla
